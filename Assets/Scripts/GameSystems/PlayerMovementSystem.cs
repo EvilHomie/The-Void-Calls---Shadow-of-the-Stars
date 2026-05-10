103ac88d44be309b1c2682c41b0b7e1bd1f9fd0f
EvilHomie <pavlenkods1988@gmail.com>
2026-05-10 08:17:51 +0400

    Добавлены новые текстуры

--- Assets/Scripts/GameSystems/PlayerMovementSystem.cs
+++ Assets/Scripts/GameSystems/PlayerMovementSystem.cs
@@ -207,7 +207,8 @@ namespace GameSystems
             if (Mathf.Abs(angularVelocity) > maxSpeed || !targetOutSideShip) // только гашение если больше максимального или мышка на корабле
             {
                 angularVelocity = Mathf.MoveTowards(angularVelocity, 0f, maxSpeed * fixedDT);
-                movementRuntimeData.RotatePower = -Mathf.Sign(angularVelocity);
+                //movementRuntimeData.RotatePower = -Mathf.Sign(angularVelocity);
+                movementRuntimeData.RotatePower = Mathf.Abs(angularVelocity) < EPS ? 0f : angularVelocity / maxSpeed;
                 return;
             }
 
